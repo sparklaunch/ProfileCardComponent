b9593b8c5a0953b9f0a3d0eecbcbb0a7609a9c4c
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-25 11:49:47 +0900

    Initial Commit

---
+++ Shared/ProfileCardComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCardComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCardComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
