7278b40c1617bed41551bb9ea718a82f12cac0cc
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-26 13:45:36 +0900

    Add the profile photo.

--- Shared/Components/Card.swift
+++ Shared/Components/Card.swift
@@ -9,6 +9,18 @@ import SwiftUI
 
 struct Card: View {
     var body: some View {
-        Text("hello")
+        VStack {
+            Color("CardColor")
+                .frame(height: 100)
+            Image("Victor")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+                .overlay(Circle()
+                    .stroke(lineWidth: 5)
+                    .foregroundColor(.white))
+        }
+        .padding(.horizontal, 30)
+        .scaledToFit()
     }
 }
